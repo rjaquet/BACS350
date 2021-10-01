a57528a3a0272f9da503cb58575017637be8e7fe
rjaquet <89422179+rjaquet@users.noreply.github.com>
2021-10-01 15:28:44 -0600

    Fix hello app

--- week2/Hello/config/urls.py
+++ week2/Hello/config/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
+def homePageView(request):
+    return HttpResponse('Hello World')
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homePageView),
 ]
