b0c2f458aad810b05176261c60e68b50c281a264
rjaquet <89422179+rjaquet@users.noreply.github.com>
2021-10-08 15:37:24 -0600

    template view

---
+++ week3/Pages/config/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('about', TemplateView.as_view(template_name="about.html")),
+    path('home', TemplateView.as_view(template_name="home.html")),
+]
