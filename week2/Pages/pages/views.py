45022df91b3a70cff355b2afab869cecf3974141
rjaquet <89422179+rjaquet@users.noreply.github.com>
2021-10-15 15:19:56 -0600

    templates

--- week2/Pages/pages/views.py
+++ week2/Pages/pages/views.py
@@ -1,8 +1,22 @@
-from django.http import HttpResponse
-from django.views.generic import TemplateView
+class AboutView(TemplateView):
+    template_name = 'page.html'
+    
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'About this Class', 
+            'body': 'Once upon a time ...',
+        }
+ 
+class HomeView(TemplateView):
+    template_name = "page.html"
+    
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'My Home Page', 
+            'body': 'This page is boring ...',
+        }
+ 
 
-def homePageView(request):
-    return HttpResponse('<h1>Hello World</h1>')
-
-class MyView(TemplateView):
+class IndexView(TemplateView):
     template_name = "index.html"
+
