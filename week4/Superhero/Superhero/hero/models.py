b1bd25b13252a4178c213a29a2489faafa0a4bd0
rjaquet <89422179+rjaquet@users.noreply.github.com>
2021-10-27 16:35:32 -0600

    copied code

---
+++ week4/Superhero/Superhero/hero/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Hero (models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
